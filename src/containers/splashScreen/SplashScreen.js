ba3340da80a385a61cc6f359ba69a288306cf649
JuhiLasod <juhilasod29@gmail.com>
2025-06-13 18:48:38 +0530

    all files added together

---
+++ src/containers/splashScreen/SplashScreen.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import "./SplashScreen.css";
+import { greeting, splashScreen } from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function SplashScreen() {
+  const { isDark } = useContext(StyleContext);
+  
+  return (
+    <div className={isDark ? "dark-mode splash-container" : "splash-container"}>
+      <div className="splash-animation-container">
+        <img
+          src={splashScreen.animation}
+          alt="Splash Animation"
+          className="splash-gif"
+        />
+      </div>
+      <div className="splash-title-container">
+        {/* <span className="grey-color"> &lt;</span> */}
+        <span className="splash-title">{greeting.username}</span>
+        {/* <span className="grey-color">/&gt;</span> */}
+      </div>
+    </div>
+  );
+}
